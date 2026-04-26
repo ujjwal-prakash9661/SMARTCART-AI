1d4fde50b524c5e035d42323634ea6fdb203d402
Student ujjwal-prakash9661 <ujjwalprakashrc11.22@gmail.com>
2026-04-26 19:48:05 +0530

    passportJs updated

--- BACKEND/src/config/passport.js
+++ BACKEND/src/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID : process.env.GOOGLE_CLIENT_ID,
             clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL : "https://smartcart-ai-vvqe.onrender.com/api/auth/google/callback/api/auth/google/callback"
+            callbackURL : "https://smartcart-ai-vvqe.onrender.com/api/auth/google/callback"
         },
 
         async (accessToken, refreshToken, profile, done) => {
